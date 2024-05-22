c7576fe6bd3dbce470184b53dec0d2c2b84c6897
zach <oset.zak@bmbsoft.com.vn>
2024-05-22 11:32:34 +0700

    fix bugs

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,7 +9,7 @@ const Header = () => {
 
   return (
     <header className="d-flex justify-between align-center p-40">
-      <Link to="/">
+      <Link to="">
         <div className="d-flex align-center">
           <img width={40} height={40} src="img/logo.png" alt="Logo" />
           <div>
